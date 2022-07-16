7bb7fc1577ae6a7c40fa19a0dfc26a7c081d3617
Ramil <ramil@ramil.ramil>
2022-07-16 10:13:18 +0300

    Update 2022-07-16T07:13:00.728Z

--- src/js/import/modules.js
+++
@@ -1,4 +0,0 @@
-import '../../../node_modules/jquery/dist/jquery.min.js';
-import '../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
-import '%modules%/header/header';
-import '%modules%/footer/footer';
